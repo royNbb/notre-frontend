cd521c9b0df91998e683100411075bb8d57a8721
Roy Nababan <roy.maruli@ui.ac.id>
2024-05-22 17:09:17 +0700

    fix: fixate category utils

--- app/utils/split-category.ts
+++ app/utils/split-category.ts
@@ -1,23 +1,3 @@
-import { CategoriesByLetter, Categories } from "../interfaces/category";
-
-export const splitCategoriesByLetter = (
-  categories: Categories
-): CategoriesByLetter => {
-  return categories.data.reduce((result, category) => {
-    const firstLetter = category.name.charAt(0).toUpperCase();
-
-    if (!result[firstLetter]) {
-      result[firstLetter] = [];
-    }
-
-    result[firstLetter].push(category);
-    result[firstLetter] = result[firstLetter].sort((a, b) =>
-      a.name.localeCompare(b.name)
-    );
-    return result;
-  }, {} as CategoriesByLetter);
-};
-
 import { CategoriesByTypeAndLetter, Categories } from "../interfaces/category";
 
 export const splitCategoriesByTypeAndLetter = (
