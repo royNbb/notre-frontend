e879bb03270b80c8653d4c7bfcef8369a9562bfa
Roy Nababan <roy.maruli@ui.ac.id>
2024-05-26 17:46:09 +0700

    fix: update navbar

--- app/components/navbar/index.tsx
+++ app/components/navbar/index.tsx
@@ -2,73 +2,75 @@
 
 import { usePathname } from "next/navigation";
 import Link from "next/link";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { twMerge } from "tailwind-merge";
 import { useSession, signOut } from "next-auth/react";
 
 const Navbar: FC = () => {
   const pathname = usePathname();
   const { data, status } = useSession();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
   return (
-    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm sticky top-0 ">
+    <header className="flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm sticky top-0">
       <nav
         className="mt-6 relative max-w-screen-xl w-full backdrop-blur-md bg-white/30 border border-gray-200 rounded-[36px] mx-2 py-3 px-4 md:flex md:items-center md:justify-between md:py-0 md:px-6 lg:px-8 xl:mx-auto"
         aria-label="Global"
       >
-        <div className='flex items-center justify-between'>
-          <Link
-            href='/'
-            className='flex-none text-xl font-semibold'
-          >
+        <div className="flex items-center justify-between">
+          <Link href="/" className="flex-none text-xl font-semibold">
             Notre
           </Link>
           <div className="md:hidden">
             <button
               type="button"
-              className="hs-collapse-toggle w-8 h-8 flex justify-center items-center text-sm font-semibold rounded-full border border-gray-200 text-gray-800 hover:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none"
-              data-hs-collapse="#navbar-collapse-with-animation"
-              aria-controls="navbar-collapse-with-animation"
+              className="w-8 h-8 flex justify-center items-center text-sm font-semibold rounded-full border border-gray-200 text-gray-800 hover:bg-gray-100"
+              onClick={toggleMenu}
               aria-label="Toggle navigation"
             >
-              <svg
-                className="hs-collapse-open:hidden flex-shrink-0 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="3" x2="21" y1="6" y2="6" />
-                <line x1="3" x2="21" y1="12" y2="12" />
-                <line x1="3" x2="21" y1="18" y2="18" />
-              </svg>
-              <svg
-                className="hs-collapse-open:block hidden flex-shrink-0 w-4 h-4"
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <path d="M18 6 6 18" />
-                <path d="m6 6 12 12" />
-              </svg>
+              {isMenuOpen ? (
+                <svg
+                  className="w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <path d="M18 6 6 18" />
+                  <path d="m6 6 12 12" />
+                </svg>
+              ) : (
+                <svg
+                  className="w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                >
+                  <line x1="3" x2="21" y1="6" y2="6" />
+                  <line x1="3" x2="21" y1="12" y2="12" />
+                  <line x1="3" x2="21" y1="18" y2="18" />
+                </svg>
+              )}
             </button>
           </div>
         </div>
 
         <div
-          id="navbar-collapse-with-animation"
-          className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block"
+          className={twMerge(
+            "overflow-hidden transition-all duration-300 basis-full grow md:block ml-1 my-2",
+            isMenuOpen ? "block" : "hidden"
+          )}
         >
           <div className="flex flex-col gap-y-4 gap-x-0 mt-5 md:flex-row md:items-center md:justify-end md:gap-y-0 md:gap-x-7 md:mt-0 md:ps-7">
             <Link
@@ -89,7 +91,6 @@ const Navbar: FC = () => {
             >
               Resources
             </Link>
-
             <Link
               href="/categories"
               className={twMerge(
@@ -99,21 +100,17 @@ const Navbar: FC = () => {
             >
               Categories
             </Link>
-
             {status === "authenticated" && (
-                <Link
-                  href="/history"
-                  className={twMerge(
-                    "font-medium text-gray-500 hover:text-gray-400 md:py-6",
-                    pathname === "/history" && "text-blue-600"
-                  )}
-                >
-                  History
-                </Link>
-              )}
-
-
-
+              <Link
+                href="/history"
+                className={twMerge(
+                  "font-medium text-gray-500 hover:text-gray-400 md:py-6",
+                  pathname === "/history" && "text-blue-600"
+                )}
+              >
+                History
+              </Link>
+            )}
             <Link
               className={twMerge(
                 "flex items-center gap-x-2 font-medium text-gray-500 hover:text-blue-600 md:border-s md:border-gray-300 md:my-6 md:ps-6",
