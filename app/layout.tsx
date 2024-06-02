09c60ec6e89d40dc73ff38ce136b0f529be3069b
Roy Nababan <roy.maruli@ui.ac.id>
2024-06-02 17:11:32 +0700

    feat: change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,16 @@
-import type { Metadata } from "next";
-import { fonts } from "./fonts";
-import "./globals.css";
-import { ChakraProviders } from "./providers/chakra-providers";
-import { twMerge } from "tailwind-merge";
-import { SWRProviders } from "./providers/swr-providers";
-import SessionProviderWrapper from "./providers/session-provider";
+// app/layout.tsx
+import { Metadata } from 'next';
+import Head from 'next/head';
+import { fonts } from './fonts';
+import './globals.css';
+import { ChakraProviders } from './providers/chakra-providers';
+import { twMerge } from 'tailwind-merge';
+import { SWRProviders } from './providers/swr-providers';
+import SessionProviderWrapper from './providers/session-provider';
 
 export const metadata: Metadata = {
-  title: "Notre",
-  description: "Sharing your learning materials has never been easier.",
+  title: 'Notre',
+  description: 'Sharing your learning materials has never been easier.',
 };
 
 export default function RootLayout({
@@ -18,8 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
-        className={twMerge(fonts.inter.className, "flex flex-col min-h-screen")}
+        className={twMerge(fonts.inter.className, 'flex flex-col min-h-screen')}
       >
         <SessionProviderWrapper>
           <ChakraProviders>
