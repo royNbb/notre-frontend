3603d258c930838e7c3cb826f1d413001cdcc8cc
Roy Nababan <roy.maruli@ui.ac.id>
2024-08-06 16:47:55 +0700

    fix: footer

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 
 const Footer: FC = () => {
   return (
-    <div className="flex-1 flex flex-col justify-end z-50">
+    <div className="flex-1 flex flex-col justify-end">
       <div className='relative min-w-[100%] max-w-[100%] h-48 md:h-80 mt-16'>
         <Image
           src='/footer-bg.svg'
@@ -13,7 +13,7 @@ const Footer: FC = () => {
           className='object-cover select-none pointer-events-none'
         />
       </div>
-      <footer className='w-full bg-violet-950 pb-10 px-4 sm:px-6 lg:px-8 mx-auto'>
+      <footer className='w-full bg-violet-950 pb-10 px-4 sm:px-6 lg:px-8 mx-auto z-50'>
         <div className='text-center'>
           <div>
             <a
